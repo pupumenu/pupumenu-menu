390c1029fde4e4ba4205a3ddf09f243729f8e121
pupumenu <pupumenu@users.noreply.github.com>
2026-08-13 19:42:11 +0800

    V10: 回退V9.2猫图+无感自动同步(固定频道)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'dish-menu-v10';
-const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './favicon.png'];
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './cat.png', './cat-v3.png', './paw.png', './favicon.png'];
 
 self.addEventListener('install', function(e) {
   self.skipWaiting();
