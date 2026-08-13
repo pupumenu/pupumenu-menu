0e55d3e4d4b63a5aea633b0fe4c5c53464a1e92f
pupumenu <pupumenu@users.noreply.github.com>
2026-08-13 21:12:47 +0800

    V18: 全新重写 - 窄底部Tab + 透明+/-按钮 + 就它了加菜单 + 新ntfy频道同步

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dish-menu-v17';
+const CACHE_NAME = 'dish-menu-v18';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './cat.png', './cat-v3.png', './paw.png', './favicon.png'];
 
 self.addEventListener('install', function(e) {
